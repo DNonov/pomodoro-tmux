48e409ff9da6c745b0dc7641457ba2ab75e5a46d
Dimitar Nonov <d.nonov@gmail.com>
2021-10-12 22:57:57 +0100

    Rename some tests

--- test/test_pomodoro.py
+++ test/test_pomodoro.py
@@ -8,38 +8,38 @@ def test_if_end_of_work_is_setted_on_start():
 
     assert p.work_end_time_point is not None
 
-def test_check_pomodoro_status_right_after_start():
+def test_if_pomodoro_status_is_correct_after_start():
     p = Pomodoro()
     p.start_work()
 
     assert p.status() == "Work: 24:59"
 
-def test_check_rest_status_right_after_start():
+def test_if_rest_status_is_correct_after_start():
     p = Pomodoro()
     p.start_rest()
 
     assert p.status() == "Rest: 04:59"
 
-def test_reset_time_points_after_call_start_rest():
+def test_if_reset_time_points_are_reset_after_start_rest_call():
     p = Pomodoro()
     p.start_work()
     p.start_rest()
 
     assert p.work_start_time_point is None
 
-def test_reset_time_points_after_call_start_work():
+def test_if_reset_time_points_are_reset_after_start_work_call():
     p = Pomodoro()
     p.start_rest()
     p.start_work()
 
     assert p.rest_start_time_point is None
 
-def test_status_return_no_active_session():
+def test_if_status_return_no_active_session():
     p = Pomodoro()
 
     assert p.status() == "No active session!"
 
-def test_after_end_of_work_session_display_right_message():
+def test_if_after_end_of_work_session_display_correct_message():
     p = Pomodoro(work_time_seconds=1)
     p.start_work()
 
@@ -47,7 +47,7 @@ def test_after_end_of_work_session_display_right_message():
 
     assert p.status() == "Work session finished!"
 
-def test_after_end_of_rest_session_display_right_message():
+def test_if_after_end_of_rest_session_display_correct_message():
     p = Pomodoro(rest_time_seconds=1)
     p.start_rest()
 
