e839a77b2620fd42c0c1c103646aaef1c60e7d56
Dimitar Nonov <d.nonov@gmail.com>
2021-10-12 22:23:34 +0100

    Use the pomodoro object in the server

--- server.py
+++ server.py
@@ -1,15 +1,29 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from lib.Pomodoro import Pomodoro
 import time
 
 host_name = "localhost"
 server_port = 9876
+pomodoro = Pomodoro()
 
 class PomodoroServer(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def response(self, payload):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes(f'{}', "utf-8"))
+        self.wfile.write(bytes(f'{payload}', "utf-8"))
+
+    def do_GET(self):
+        if self.path == "/status":
+            self.response(pomodoro.status())
+
+        if self.path == "/start-work":
+            pomodoro.start_work()
+            self.response('Work started')
+
+        if self.path == "/start-rest":
+            pomodoro.start_rest()
+            self.response("Rest started")
 
 if __name__ == "__main__":
     web_server = HTTPServer((host_name, server_port), PomodoroServer)
