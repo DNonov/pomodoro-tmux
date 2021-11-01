10a3c10fa1839b36f9bb17fc942b00cfc4d349d6
Dimitar Nonov <d.nonov@gmail.com>
2021-11-01 22:27:48 +0000

    Add some comments to Alarm class

--- lib/Alarm.py
+++ lib/Alarm.py
@@ -1,11 +1,17 @@
 import os
 
 class Alarm():
+    """
+    It notifies the user with a sound signal.
+
+    This is functional only on linux systems with 'sox' installed.
+    """
     def __init__(self):
         self.counter = 0
         self.alarm_comand = 'play -nq -t alsa synth 0.1 sine 170'
 
     def beep(self):
+        """ Creates sound signal """
         if self.counter < 5:
             os.system(self.alarm_comand)
             self.counter += 1
